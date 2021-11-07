4c916b999b425f0bfb77ff60c0347b2756acf252
Pyry-peetri Piironen <ppiirone@c1r4p2.hive.fi>
2021-11-07 15:16:52 +0200

    Ft_memcpy and ft_memset done.

---
+++ ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppiirone <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/07 12:22:46 by ppiirone          #+#    #+#             */
+/*   Updated: 2021/11/07 12:22:49 by ppiirone         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	size_t			i;
+	unsigned char	*dest;
+	unsigned char	*srce;
+
+	i = 0;
+	dest = (unsigned char*)dst;
+	srce = (unsigned char*)src;
+	while (i < n)
+	{
+		*dest = *srce;
+		i++;
+		dest++;
+		srce++;
+	}
+	return (dst);
+}
