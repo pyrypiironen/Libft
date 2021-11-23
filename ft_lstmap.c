d57798382bfd25ee380da34944518bdc30f48755
Pyry-peetri Piironen <ppiirone@c2r5p4.hive.fi>
2021-11-23 17:13:56 +0200

    Bonus done.

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,7 +12,18 @@
 
 #include "libft.h"
 
-//t_list	*ft_lstmap(t_list *lst, t_list * (*f)(t_list *elem))
-//{
+t_list	*ft_lstmap(t_list *lst, t_list * (*f)(t_list *elem))
+{
+	t_list	*fresh;
+	t_list	*tmp;
 
-//}
+	tmp = (*f)(lst);
+	fresh = tmp;
+	while (lst->next != NULL)
+	{
+		tmp->next = (*f)(lst->next);
+		tmp = tmp->next;
+		lst = lst->next;
+	}
+	return (fresh);
+}
